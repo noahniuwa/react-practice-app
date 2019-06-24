dbe8b66fe55f5e9d2c41c4dbe496042879360b30
Noah Maizels <noahmaizels@Noah.local>
2019-06-24 21:31:27 +0800

    added build files

---
+++ build/precache-manifest.240ea14bf12c2d6639d9b308f42f5905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6396933b943837d11e3bd4f3cf8aa5b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00de5e86226aa20980a4",
+    "url": "/static/css/main.50fa15f8.chunk.css"
+  },
+  {
+    "revision": "b5e2f4052501b6399d60",
+    "url": "/static/js/2.ae012cea.chunk.js"
+  },
+  {
+    "revision": "00de5e86226aa20980a4",
+    "url": "/static/js/main.9f5444c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
